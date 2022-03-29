fe264b16185e99fe0862ea97d8a269318e58eddd
vitormalencar <vitor.maia.alencar@gmail.com>
2022-03-29 19:46:04 +0200

    updating stores

--- rtk-testing/src/app/store.ts
+++ rtk-testing/src/app/store.ts
@@ -1,12 +1,9 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
-import loginReducer from "../features/login/LoginSlice";
+
 import homeReducer from "../features/home/HomeSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    login: loginReducer,
     home: homeReducer,
   },
 });
